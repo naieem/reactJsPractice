ed7071b0bf02a2e6a8cbd286515b15e9c61645ef
naieemsupto <naieem.mahmud@selise.ch>
2017-05-18 18:07:06 +0600

    data get from external resourse

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Request from 'superagent';
+import _ from 'lodash';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+  constructor(){
+    super();
+    this.state={};
+    this.numbers = [1, 2, 3, 4, 5];
+    this.listItems = this.numbers.map((number) =>
+      <li>{number}</li>
     );
   }
+    componentWillMount(){
+      var url="https://jsonplaceholder.typicode.com/posts";
+      Request.get(url).then((response)=>{
+        console.log(response);
+        this.setState({
+          info:response.body
+        });
+      });
+    };
+    render() {
+      setTimeout(()=>{
+        this.setState({name:"naieem"});
+      }, 3000);
+      var movies=_.map(this.state.info,(movie)=>{
+        return <li>{movie.title}</li>;
+      });
+      return ( 
+        <div className="App">
+        {this.listItems}
+        {movies}
+          <div className="App-header">
+          <img src={ logo }
+          className="App-logo"
+          alt="logo" />
+          <h2> Welcome {this.state.name}!!!!!! </h2> </div> <p className="App-intro">
+          To ge started, edit <code> src / App.js </code> and save to reload. </p> </div>
+      );
+    }
 }
 
 export default App;
